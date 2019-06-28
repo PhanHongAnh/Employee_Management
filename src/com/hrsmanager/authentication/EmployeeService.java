f19ebacf958dce55fb06e91b22979384bda7274f
PhanHongAnh <honganh.p256@gmail.com>
2019-06-28 14:01:10 +0700

    config

--- src/com/hrsmanager/authentication/EmployeeService.java
+++ src/com/hrsmanager/authentication/EmployeeService.java
@@ -3,17 +3,19 @@ package com.hrsmanager.authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.hrsmanager.dao.EmployeeDAO;
 import com.hrsmanager.model.EmployeeInfo;
 
 @Service
+@Transactional
 public class EmployeeService {
 
 	@Autowired
 	private EmployeeDAO employeeDAO;
 	
-	public EmployeeInfo loadEmployeeByID(Integer employee_id){
+	public EmployeeInfo findEmployeeInfo(Integer employee_id){
 		EmployeeInfo emp = employeeDAO.findEmployeeInfo(employee_id);
 		
 		if (emp == null) {
